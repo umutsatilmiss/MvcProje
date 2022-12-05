bf0cbc2b7a56db5aa61576369cf121f190305630
66 <umutsatilmis66@gmail.com>
2022-12-05 14:03:06 +0300

    Sertifika Görev 2/4

--- DataAccesLayer/Concrete/Repositories/WriterRepository.cs
+++ DataAccesLayer/Concrete/Repositories/WriterRepository.cs
@@ -20,6 +20,11 @@ namespace DataAccesLayer.Concrete.Repositories
             throw new NotImplementedException();
         }
 
+        public Writer Get(Expression<Func<Writer, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
         public void Insert(Writer p)
         {
             throw new NotImplementedException();
---
+++ MvcProjeKampi/Controllers/IstatistikController.cs
@@ -0,0 +1,30 @@
+﻿using DataAccesLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class IstatistikController : Controller
+    {
+        Context context = new Context();
+
+        public ActionResult Index()
+        {
+            var result = context.Categories.Count();
+            var result2 = context.Headings.Count(x => x.Category.CategoryID == 7);
+            var result3 = context.Writers.Count(x => x.WriterName.Contains("a"));
+            var result4 = context.Headings.Max(x => x.Category.CategoryName);
+            var result5 = context.Categories.Count(x => x.CategoryStatus == true);
+            var result6 = context.Categories.Count(x => x.CategoryStatus == false);
+            ViewBag.CategoryCount = result;
+            ViewBag.Heading = result2;
+            ViewBag.Writer = result3;
+            ViewBag.HeadingMax = result4;
+            ViewBag.StatüsFark = (result5 - result6);
+            return View();
+        }
+    }
+}
